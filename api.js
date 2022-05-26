82f65a29cad0dbb8b5f7b0f2558313f3778a2099
Anand Kumar Nishad <anandkumar.nishad25@.com>
2022-05-26 12:54:05 +0530

    initial commit

---
+++ api.js
@@ -0,0 +1,45 @@
+// const express = require("express");
+const jsondata = require("./connection");
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use(function (req, res, next) {
+    const corsWhiteList = [
+        "http://localhost:3000",
+        "http://localhost:3001"
+    ]
+
+    if(corsWhiteList.indexOf(req.header.origin) !== -1){
+        
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        //res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+    }
+    next();
+});
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.get("/", (res, req) => {
+    if(jsondata === ""){
+        console.log("Data Not Found")
+        res.sendStaus(500)
+    } else {
+        res.send(jsondata)
+    }
+})
---
+++ connection.js
@@ -0,0 +1,7 @@
+const data = require('./data.json');
+
+const connec = () => {
+    return (data.data);
+}
+
+module.exports = connec();
