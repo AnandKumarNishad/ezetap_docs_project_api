f9a1378f638cec54a815e5aafa42b5d50ed6619e
Anand Kumar Nishad <anandkumar.nishad25@.com>
2022-05-26 13:09:14 +0530

    initial commit

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const jsondata = require("./connection");
+const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 
 const app = express()
 app.use(bodyParser.json())
